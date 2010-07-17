5c3d32bfd09bd4994f36cdbd1a5609c375d8e8b0
Gavin Gilmour <gavin.gilmour@gmail.com>
2010-07-17 17:36:22 +0100

    made date format match error log. added deploy task

--- src/net/brokentrain/bandsaw/log4j/Log4jDate.java
+++ src/net/brokentrain/bandsaw/log4j/Log4jDate.java
@@ -1,12 +1,15 @@
 package net.brokentrain.bandsaw.log4j;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 import org.apache.log4j.spi.LoggingEvent;
 
 public class Log4jDate implements Log4jItem {
 
     private LoggingEvent le;
+    
+    private static final SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
     public Log4jDate(LoggingEvent event) {
         le = event;
@@ -16,7 +19,7 @@ public class Log4jDate implements Log4jItem {
      * @see net.brokentrain.bandsaw.log4j.Log4jItem#getText()
      */
     public String getText() {
-        return new Timestamp(le.timeStamp).toString();
+        return format.format(new Timestamp(le.timeStamp));
     }
 
 }
