d50fdb34d8781c788a93e9b38e444133f4d01bf3
Gavin Gilmour <gavin.gilmour@gmail.com>
2010-07-18 21:37:17 +0100

    removed log4php bundle

--- goodies/test/test.php
+++ goodies/test/test.php
@@ -9,10 +9,15 @@
     // $logger = Logger::getLogger("Log4PHPTestLogger");
 
     $message = (!isset($argv[1])) ? md5(rand(0,10)) : implode(' ', array_slice($argv, 1));
-
+//select * from masterqueue limit 20
     $logger->info($message);
     $logger->warn($message);
     $logger->debug($message);
-    $logger->debug(new Exception("asdsad"));
+
+    try {
+        throw new Exception("yeah");
+    } catch (Exception $e) {
+        $logger->error("hoho", $e);
+    }
 
 ?>
